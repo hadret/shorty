0f0f312229caea30436fe6d89c5a9047520fe4f1
Filip Chabik <filip.chabik@samedi.de>
2022-05-29 18:01:01 +0200

    feat: allow for both upper and lower case strings for the key.

--- shortener_app/keygen.py
+++ shortener_app/keygen.py
@@ -5,7 +5,7 @@ from . import crud
 
 
 def create_random_key(length: int = 5) -> str:
-    chars = string.ascii_uppercase + string.digits
+    chars = string.ascii_letters + string.digits
     return "".join(secrets.choice(chars) for _ in range(length))
 
 
