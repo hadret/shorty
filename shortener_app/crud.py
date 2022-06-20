0bfe9d35f2ff32a7e70e657f2b8e8744884a13cf
Filip Chabik <hadret@hey.com>
2022-06-20 15:46:43 +0200

    🔥 Ditched -> modules.URL that were breaking LSP

--- shortener_app/crud.py
+++ shortener_app/crud.py
@@ -16,7 +16,7 @@ def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
     return db_url
 
 
-def get_db_url_by_key(db: Session, url_key: str) -> models.URL:
+def get_db_url_by_key(db: Session, url_key: str):
     return (
         db.query(models.URL)
         .filter(models.URL.key == url_key, models.URL.is_active)
@@ -24,7 +24,7 @@ def get_db_url_by_key(db: Session, url_key: str) -> models.URL:
     )
 
 
-def get_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
+def get_db_url_by_secret_key(db: Session, secret_key: str):
     return (
         db.query(models.URL)
         .filter(models.URL.secret_key == secret_key, models.URL.is_active)
@@ -32,14 +32,14 @@ def get_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
     )
 
 
-def update_db_clicks(db: Session, db_url: schemas.URL) -> models.URL:
+def update_db_clicks(db: Session, db_url: schemas.URL):
     db_url.clicks += 1
     db.commit()
     db.refresh(db_url)
     return db_url
 
 
-def deactivate_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
+def deactivate_db_url_by_secret_key(db: Session, secret_key: str):
     db_url = get_db_url_by_secret_key(db, secret_key)
     if db_url:
         db_url.is_active = False
