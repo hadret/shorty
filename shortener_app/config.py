84b518728609ba0e8c40850de544c59f9bf5a22f
Filip Chabik <hadret@gmail.com>
2024-01-31 13:50:58 +0100

    Bumped Poetry dependencies, including FastAPI and Pydantic major version

--- shortener_app/config.py
+++ shortener_app/config.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
--- shortener_app/schemas.py
+++ shortener_app/schemas.py
@@ -11,7 +11,7 @@ class URL(URLBase):
     clicks: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class URLInfo(URL):
